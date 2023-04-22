6ac80c1fc22264f8c335374b15fcb331fd22c8fd
SUNWUYUAN <1847261658@qq.com>
2023-04-22 13:10:44 +0800

    1

--- themes/anzhiyu/scripts/helpers/inject_head_js.js
+++ themes/anzhiyu/scripts/helpers/inject_head_js.js
@@ -8,6 +8,9 @@
 hexo.extend.helper.register("inject_head_js", function () {
   const { darkmode, aside } = this.theme;
 
+  const start = darkmode.start ? darkmode.start : 6;
+  const end = darkmode.end ? darkmode.end : 18;
+
   const { theme_color } = hexo.theme.config;
   const themeColorLight = (theme_color && theme_color.enable && theme_color.meta_theme_color_light) || "#ffffff";
   const themeColorDark = (theme_color && theme_color.enable && theme_color.meta_theme_color_dark) || "#0d0d0d";
@@ -60,6 +63,23 @@ hexo.extend.helper.register("inject_head_js", function () {
     })
   `;
 
+  const getCSS = `
+    win.getCSS = (url,id = false) => new Promise((resolve, reject) => {
+      const link = document.createElement('link')
+      link.rel = 'stylesheet'
+      link.href = url
+      if (id) link.id = id
+      link.onerror = reject
+      link.onload = link.onreadystatechange = function() {
+        const loadState = this.readyState
+        if (loadState && loadState !== 'loaded' && loadState !== 'complete') return
+        link.onload = link.onreadystatechange = null
+        resolve()
+      }
+      document.head.appendChild(link)
+    })
+  `;
+
   let darkmodeJs = "";
   if (darkmode.enable) {
     darkmodeJs = `
@@ -91,11 +111,10 @@ hexo.extend.helper.register("inject_head_js", function () {
             if (isLightMode) activateLightMode()
             else if (isDarkMode) activateDarkMode()
             else if (isNotSpecified || hasNoSupport) {
-              // const now = new Date()
-              // const hour = now.getHours()
-              // const isNight = hour <= 6 || hour >= 18
-              // isNight ? activateDarkMode() : activateLightMode()
-              activateDarkMode()
+              const now = new Date()
+              const hour = now.getHours()
+              const isNight = hour <= ${start} || hour >= ${end}
+              isNight ? activateDarkMode() : activateLightMode()
             }
             window.matchMedia('(prefers-color-scheme: dark)').addListener(function (e) {
               if (saveToLocal.get('theme') === undefined) {
@@ -109,7 +128,7 @@ hexo.extend.helper.register("inject_head_js", function () {
       darkmodeJs += `
           const now = new Date()
           const hour = now.getHours()
-          const isNight = hour <= 6 || hour >= 18
+          const isNight = hour <= ${start} || hour >= ${end}
           if (t === undefined) isNight ? activateDarkMode() : activateLightMode()
           else if (t === 'light') activateLightMode()
           else activateDarkMode()
@@ -145,5 +164,7 @@ hexo.extend.helper.register("inject_head_js", function () {
     detectApple()
     `;
 
-  return `<script>(win=>{${localStore + getScript + darkmodeJs + asideStatus + detectApple}})(window)</script>`;
+  return `<script>(win=>{${
+    localStore + getScript + getCSS + darkmodeJs + asideStatus + detectApple
+  }})(window)</script>`;
 });
