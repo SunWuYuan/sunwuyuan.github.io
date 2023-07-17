6933a8c3b1e4974844a28a1962e5e58ac8c5eb95
SUNWUYUAN <1847261658@qq.com>
2023-07-17 14:21:06 +0800

    1

--- themes/anzhiyu/source/js/utils.js
+++ themes/anzhiyu/source/js/utils.js
@@ -58,15 +58,18 @@ const anzhiyu = {
     }
   },
 
-  snackbarShow: (text, showAction = false, duration = 2000) => {
+  snackbarShow: (text, showActionFunction = false, duration = 2000, actionText = false) => {
     const { position, bgLight, bgDark } = GLOBAL_CONFIG.Snackbar;
     const bg = document.documentElement.getAttribute("data-theme") === "light" ? bgLight : bgDark;
     const root = document.querySelector(":root");
     root.style.setProperty("--anzhiyu-snackbar-time", duration + "ms");
+
     Snackbar.show({
       text: text,
       backgroundColor: bg,
-      showAction: showAction,
+      onActionClick: showActionFunction,
+      actionText: actionText,
+      showAction: actionText,
       duration: duration,
       pos: position,
       customClass: "snackbar-css",
@@ -564,27 +567,6 @@ const anzhiyu = {
     input.focus();
     input.setSelectionRange(-1, -1);
   },
-  //友链随机传送
-  travelling() {
-    var fetchUrl = GLOBAL_CONFIG.friends_vue_info.apiurl + "randomfriend";
-    fetch(fetchUrl)
-      .then(res => res.json())
-      .then(json => {
-        var name = json.name;
-        var link = json.link;
-        Snackbar.show({
-          text:
-            "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
-          duration: 8000,
-          pos: "top-center",
-          actionText: "前往",
-          onActionClick: function (element) {
-            element.style.opacity = 0;
-            window.open(link, "_blank");
-          },
-        });
-      });
-  },
   //切换音乐播放状态
   musicToggle: function (changePaly = true) {
     if (!anzhiyu_musicFirst) {
@@ -670,7 +652,7 @@ const anzhiyu = {
   },
   // 显示中控台
   showConsole: function () {
-    document.querySelector("#console").classList.add("show");
+    consoleEl.classList.add("show");
     anzhiyu.initConsoleState();
   },
 
@@ -682,6 +664,14 @@ const anzhiyu = {
     } else if (consoleEl.classList.contains("reward-show")) {
       // 如果是打赏控制台，就关闭打赏控制台
       consoleEl.classList.remove("reward-show");
+    }
+    // 获取center-console元素
+    const centerConsole = document.getElementById("center-console");
+
+    // 检查center-console是否被选中
+    if (centerConsole.checked) {
+      // 取消选中状态
+      centerConsole.checked = false;
     }
   },
   // 取消加载动画
@@ -1047,8 +1037,16 @@ const anzhiyu = {
 
   // 跳转开往
   totraveling: function () {
-    anzhiyu.snackbarShow("即将跳转到「开往」项目的成员博客，不保证跳转网站的安全性和可用性", !1, 5000);
-    setTimeout(function () {
+    anzhiyu.snackbarShow(
+      "即将跳转到「开往」项目的成员博客，不保证跳转网站的安全性和可用性",
+      element => {
+        element.style.opacity = 0;
+        travellingsTimer && clearTimeout(travellingsTimer);
+      },
+      5000,
+      "取消"
+    );
+    travellingsTimer = setTimeout(function () {
       window.open("https://www.travellings.cn/go.html");
     }, "5000");
   },
@@ -1141,6 +1139,30 @@ const anzhiyu = {
       window.oncontextmenu = null;
     } else if (!window.oncontextmenu && oncontextmenuFunction) {
       window.oncontextmenu = oncontextmenuFunction;
+    }
+  },
+  switchConsole: () => {
+    // switch console
+    const consoleEl = document.getElementById("console");
+    //初始化隐藏边栏
+    const $htmlDom = document.documentElement.classList;
+    $htmlDom.contains("hide-aside")
+      ? document.querySelector("#consoleHideAside").classList.add("on")
+      : document.querySelector("#consoleHideAside").classList.remove("on");
+    if (consoleEl.classList.contains("show")) {
+      consoleEl.classList.remove("show");
+    } else {
+      consoleEl.classList.add("show");
+    }
+    const consoleKeyboard = document.querySelector("#consoleKeyboard");
+    if (consoleKeyboard) {
+      if (localStorage.getItem("keyboardToggle") === "true") {
+        consoleKeyboard.classList.add("on");
+        anzhiyu_keyboard = true;
+      } else {
+        consoleKeyboard.classList.remove("on");
+        anzhiyu_keyboard = false;
+      }
     }
   },
   // 定义 intersectionObserver 函数，并接收两个可选参数
